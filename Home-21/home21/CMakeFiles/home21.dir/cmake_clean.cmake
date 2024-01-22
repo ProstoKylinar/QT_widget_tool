e6325a9b8cb754c188db9334516e9bce90492c68
ProstoKylinar <102613624+ProstoKylinar@users.noreply.github.com>
2024-01-22 16:15:40 +0300

    Add files via upload

---
+++ Home-21/home21/CMakeFiles/home21.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/home21.dir/main.cpp.o"
+  "home21"
+  "home21.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/home21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
